4c0cb94b411db65aae561d64cad1a5d718778c35
urvd <uriel.vido@hotmail.fr>
2025-02-04 00:50:10 +0100

    init routing, header, nav

---
+++ src/app/default/default.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default',
+  imports: [],
+  templateUrl: './default.component.html',
+  styleUrl: './default.component.css'
+})
+export class DefaultComponent {
+
+}
