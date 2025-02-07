25e51174ec3417fd7e1e207202102ab66f1866ec
urvd <uriel.vido@hotmail.fr>
2025-02-07 11:02:29 +0100

    fix classement model

---
+++ src/app/services/foot/match.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { DatafootService } from '../datafoot.service';
+import { Equipe } from '../../models/sport.model';
+import { Etape, Match } from '../../models/match.models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MatchService {
+
+}
